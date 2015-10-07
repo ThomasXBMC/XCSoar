22b346a6867ace54ddbf850d078323540e4fbe6e
Thomas Ruf <Thomas.Ruf@computer.org>
2015-10-07 20:55:31 +0200

    Add Show IP address to KoboMenu/Network

--- src/Kobo/NetworkDialog.cpp
+++ src/Kobo/NetworkDialog.cpp
@@ -21,6 +21,8 @@ Copyright_License {
 }
 */
 
+#include "Look/DialogLook.hpp"
+#include "Screen/TextWindow.hpp"
 #include "NetworkDialog.hpp"
 #include "WifiDialog.hpp"
 #include "Dialogs/WidgetDialog.hpp"
@@ -46,9 +48,13 @@ class NetworkWidget final
     WIFI,
     TELNET,
     FTP,
+    SHOW_IP,
   };
 
   Button *toggle_wifi_button, *wifi_button;
+
+  TextWindow ip_window;
+  PixelRect ip_rc;
 
 public:
   NetworkWidget(const DialogLook &look):RowFormWidget(look) {}
@@ -61,6 +67,8 @@ public:
 
 private:
   void ToggleWifi();
+  void ShowIPAddress();
+  bool GetMyIPAddress(char *addr,unsigned size);
 
   /* virtual methods from class ActionListener */
   virtual void OnAction(int id) override;
@@ -85,6 +93,23 @@ NetworkWidget::Prepare(ContainerWindow &parent, const PixelRect &rc)
 
   AddButton(_T("Ftp server"), *this, FTP);
 
+  AddButton(_T("Show IP Address"), *this, SHOW_IP);
+
+  const DialogLook &look = UIGlobals::GetDialogLook();
+
+  /* create text window for IP address, set invisible */
+  ip_rc.left = rc.right/2 - 60 ;
+  ip_rc.top = rc.bottom/2 - 20;
+  ip_rc.right = rc.right/2 + 40;
+  ip_rc.bottom = ip_rc.top + 50;
+
+  ip_window.Create(parent,nullptr,ip_rc);
+#ifndef USE_GDI
+  ip_window.SetFont(look.bold_font);
+#endif
+  //ip_window.MoveToCenter();
+  ip_window.SetVisible(false);
+
   UpdateButtons();
 }
 
@@ -98,6 +123,22 @@ NetworkWidget::ToggleWifi()
   }
 
   UpdateButtons();
+}
+
+void
+NetworkWidget::ShowIPAddress()
+{
+  char addr[64];
+  char text[128];
+  strcpy(text,"My IP Address: ");
+
+  if (GetMyIPAddress(addr,sizeof(addr)))
+    strcat(text,addr);
+  else
+    strcat(text,"<Unknown>");
+
+  ip_window.set_text(text);
+  ip_window.ShowOnTop();
 }
 
 void
@@ -119,6 +160,10 @@ NetworkWidget::OnAction(int id)
   case FTP:
     KoboRunFtpd();
     break;
+
+  case SHOW_IP:
+    ShowIPAddress();
+    break;
   }
 }
 
@@ -132,4 +177,72 @@ ShowNetworkDialog()
   dialog.AddButton(_("Close"), mrOK);
   dialog.ShowModal();
   dialog.StealWidget();
+}
+
+
+/**
+ * get the IP address of interface eth0
+ */
+bool
+NetworkWidget::GetMyIPAddress(char *addr,unsigned size)
+{
+#if 0
+
+#include "OS/FileUtil.hpp"
+#include "OS/Process.hpp"
+
+  const char *path_script = "/tmp/xcsoar.script.XXX";
+  const char *path_result = "/tmp/xcsoar.result.XXX";
+
+  char *pos, *end;
+  char result[128];
+  char script[128];
+
+  /* build the script string */
+  // ifconfig | sed -n '/inet addr/s/.*inet addr:\([0-9.]*\).*/\1/p'
+  strcpy(script,"ifconfig | grep 'inet addr:' > ");
+  strcat(script,path_result);
+
+  /* delete the files, write the script file */
+  File::Delete(path_result);
+  File::Delete(path_script);
+  if(!File::CreateExclusive(path_script) ||
+      !File::WriteExisting(path_script,script))
+    return false;
+
+  /* run the script and read the result */
+  if (!Run("/bin/sh",path_script))
+    return false;
+  if (!File::ReadString(path_result,result,sizeof(result)))
+    return false;
+
+  /* scan result and copy to addr */
+  if (!(pos = strstr(result,"inet addr:")))
+    return false;
+  pos += 10;
+  if (!(end = strchr(pos,' ')) || (unsigned(end-pos) >= size))
+      return false;
+  *end = 0;
+
+  strcpy(addr,pos);
+
+  return true;
+#else
+  FILE *pipe;
+
+  /* call ifconfig and filter output with sed */
+  if (!(pipe = popen("ifconfig | sed -n 's/.*inet addr:\\([0-9.]*\\).*/\\1/p'","r")))
+    return false;
+
+  /* get line from pipe, remove trailing newline, found if not local ip */
+  while (fgets(addr, size, pipe)) {
+    addr[strcspn(addr, "\n")] = 0;
+    if (strcmp(addr,"127.0.0.1")) {
+      pclose(pipe);
+      return true;
+    }
+  }
+  pclose(pipe);
+  return false;
+#endif
 }
